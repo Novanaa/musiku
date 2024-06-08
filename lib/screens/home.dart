a6690c67dede456d2401a444e150d02d9a44cd8e
Novanaa <kadeknovacahayaputra@gmail.com>
2024-06-08 09:35:45 -0400

    Added application header

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:musiku/constants/color.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text("home")));
+    return Scaffold(
+      appBar: header(),
+    );
+  }
+
+  AppBar header() {
+    return AppBar(
+      elevation: 0,
+      backgroundColor: ColorConstants.backgroundColor,
+      actions: [
+        IconButton(
+            onPressed: () {},
+            icon: SvgPicture.asset("assets/icons/menu.svg",
+                width: 25, height: 25))
+      ],
+      titleTextStyle: const TextStyle(
+          color: ColorConstants.textColor,
+          fontFamily: "Poppins",
+          fontWeight: FontWeight.w700,
+          fontSize: 16),
+      title: const Text("Musiku"),
+    );
   }
 }
