e5d73cdc2a7460c791a6ef9923ba244467a62ef8
Novaa <130564362+Novanaa@users.noreply.github.com>
2024-06-08 06:33:03 -0400

    [MUS-13] Add Application Bottom Navigation Bar (#10)
    * Added flutter svg packages
    
    * Added application layout widget
    
    * Added icon color prop
    
    * Defined application assets
    
    * Added outline and solid folder svg
    
    * Added outline and solid home svg
    
    * Added outline and solid search icon svg
    
    * Added outline and solid media library icon svg
    
    * Added application layout screen
    
    * Added application home screen
    
    * Added application search screen
    
    * Added application directories screen
    
    * Added application collection screen

---
+++ lib/screens/collection.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CollectionScreen extends StatelessWidget {
+  const CollectionScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: Center(child: Text("collection")));
+  }
+}
