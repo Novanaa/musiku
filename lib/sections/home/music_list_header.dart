94ec10f6fd67f5d174b5480b3a10323d2f1f0fe8
Novanaa <kadeknovacahayaputra@gmail.com>
2024-06-09 11:38:25 -0400

    Added home section music list header

--- lib/sections/home/music_list_header.dart
+++ lib/sections/home/music_list_header.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:musiku/constants/color.dart';
+import 'package:touch_ripple_effect/touch_ripple_effect.dart';
 
 class MusicListHeader extends StatelessWidget {
   const MusicListHeader({super.key});
@@ -9,67 +10,33 @@ class MusicListHeader extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.only(left: 15, right: 15),
-      child: SingleChildScrollView(
-        scrollDirection: Axis.horizontal,
-        child: Row(
-          children: [
-            musicHeaderListItem(
-                "Leave a star!",
-                "Hey there! could you give me a star? i will appreciate that!",
-                "assets/icons/sparkles.svg"),
-            const SizedBox(
-              width: 12,
-            ),
-            musicHeaderListItem("Report some bugs!",
-                "Have an issues while using Musiku?", "assets/icons/bug.svg"),
-            const SizedBox(
-              width: 12,
-            ),
-            musicHeaderListItem(
-                "Suggest a feature?",
-                "Have an idea? contribute to Musiku app development!",
-                "assets/icons/file-json.svg"),
-          ],
-        ),
+      padding: const EdgeInsets.only(top: 15, bottom: 10),
+      decoration: const BoxDecoration(
+          border:
+              Border(bottom: BorderSide(color: ColorConstants.borderColor))),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          musicListHeaderText(),
+          musicListHeaderListOptions(),
+        ],
       ),
     );
   }
 
-  Container musicHeaderListItem(
-      String title, String description, String assetName) {
-    return Container(
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(8),
-          color: ColorConstants.cardBackgroundColor),
-      padding: const EdgeInsets.all(12),
-      width: 230,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          SvgPicture.asset(
-            assetName,
-            height: 32,
-            width: 32,
-          ),
-          const SizedBox(
-            height: 5,
-          ),
-          Text(
-            title,
-            style: const TextStyle(fontWeight: FontWeight.w600, fontSize: 16),
-          ),
-          const SizedBox(
-            height: 5,
-          ),
-          Opacity(
-            opacity: 0.8,
-            child: Text(
-              description,
-              style: const TextStyle(fontSize: 12),
-            ),
-          )
-        ],
-      ),
-    );
+  TouchFeedback musicListHeaderListOptions() {
+    return TouchFeedback(
+        feedbackDuration: const Duration(milliseconds: 200),
+        rippleColor: ColorConstants.rippleColor,
+        // Placeholder for right now
+        onTap: () {},
+        child: SvgPicture.asset(
+          "assets/icons/list-options.svg",
+        ));
+  }
+
+  Text musicListHeaderText() {
+    return const Text("Listen to everything!",
+        style: TextStyle(fontWeight: FontWeight.w600, fontSize: 13.5));
   }
 }
