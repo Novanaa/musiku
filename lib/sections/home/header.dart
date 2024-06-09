a6f1c5e9ee5e596b5f05fb4e69f2d70f94d2e965
Novanaa <kadeknovacahayaputra@gmail.com>
2024-06-09 04:45:48 -0400

    Added home header section

---
+++ lib/sections/home/header.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:musiku/constants/color.dart';
+
+class HomeScreenHeader extends StatelessWidget implements PreferredSizeWidget {
+  final VoidCallback openDrawer;
+
+  const HomeScreenHeader({super.key, required this.openDrawer});
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      elevation: 0,
+      backgroundColor: ColorConstants.backgroundColor,
+      actions: [
+        IconButton(
+            onPressed: openDrawer,
+            icon: SvgPicture.asset("assets/icons/menu.svg",
+                width: 25, height: 25))
+      ],
+      titleTextStyle: const TextStyle(
+          color: ColorConstants.textColor,
+          fontFamily: "Poppins",
+          fontWeight: FontWeight.w700,
+          fontSize: 16),
+      title: const Text("Musiku"),
+    );
+  }
+}
