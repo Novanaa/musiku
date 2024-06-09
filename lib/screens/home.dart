8675aecabc1194e261c70086955f82443b523953
Novanaa <kadeknovacahayaputra@gmail.com>
2024-06-09 10:37:33 -0400

    Implemented home screen music list header widget

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:musiku/constants/color.dart';
 import 'package:musiku/sections/home/drawer.dart';
 import 'package:musiku/sections/home/header.dart';
+import 'package:musiku/sections/home/music_list_header.dart';
 
 class HomeScreen extends StatelessWidget {
   HomeScreen({super.key});
@@ -17,6 +18,9 @@ class HomeScreen extends StatelessWidget {
         appBar: HomeScreenHeader(
             openDrawer: () => _key.currentState?.openEndDrawer()),
         endDrawer: const HomeScreenDrawer(),
+        body: ListView(
+          children: const [MusicListHeader()],
+        ),
       ),
     );
   }
