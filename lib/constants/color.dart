1ec721c5be8dbe6756525c8cf5f311f07d80c08c
Novanaa <kadeknovacahayaputra@gmail.com>
2024-06-09 09:32:55 -0400

    Updated modal background color value

--- lib/constants/color.dart
+++ lib/constants/color.dart
@@ -6,7 +6,7 @@ class ColorConstants {
   static const Color iconColor = Color(0xffFEFEFE);
   static const Color borderColor = Color(0xff656569);
   static const Color inputColor = Color(0xff333336);
-  static const Color modalBackgroundColor = Color(0xff191919);
+  static const Color modalBackgroundColor = Color(0xff202020);
   static const Color headerBackgroundColor = Color(0xff191919);
   static const Color destructiveColor = Color(0xfffc4949);
   static const Color splashColor = Color.fromARGB(68, 254, 254, 254);
