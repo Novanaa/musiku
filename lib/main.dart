1259452f19eb2f4c8bd2ff06f41e811af2d58a0f
Novanaa <kadeknovacahayaputra@gmail.com>
2024-06-10 10:47:52 -0400

    Wrapped main widget with get material app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:musiku/constants/color.dart';
 import 'package:musiku/screens/layout.dart';
 import 'package:flutter/services.dart';
@@ -14,7 +15,7 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       home: const Layout(),
       theme: ThemeData(
