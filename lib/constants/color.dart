55fae2575ac77e0bad4290e9f37a77e3e1de3eb9
Novaa <130564362+Novanaa@users.noreply.github.com>
2024-06-10 01:22:27 -0400

    [MUS-20] Add Search Bar On Application Search Screen (#28)
    * Updated input background color value
    
    * Implemented search screen bar widget
    
    * Added missing search screen bar params
    
    * Added search bar screen section widget

--- lib/constants/color.dart
+++ lib/constants/color.dart
@@ -5,7 +5,7 @@ class ColorConstants {
   static const Color textColor = Color(0xffFEFEFE);
   static const Color iconColor = Color(0xffFEFEFE);
   static const Color borderColor = Color(0xff656569);
-  static const Color inputColor = Color(0xff333336);
+  static const Color inputColor = Color.fromARGB(255, 41, 41, 44);
   static const Color modalBackgroundColor = Color(0xff191919);
   static const Color cardBackgroundColor = Color(0xff202020);
   static const Color headerBackgroundColor = Color(0xff191919);
--- lib/screens/search.dart
+++ lib/screens/search.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:musiku/sections/search/search_bar.dart';
 
 class SearchScreen extends StatelessWidget {
   const SearchScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text("search")));
+    return Scaffold(
+      body: ListView(
+        children: [
+          SearchScreenBar(
+            // Implement later on!!
+            onTextChanged: (String text) {
+              print(text);
+            },
+          )
+        ],
+      ),
+    );
   }
 }
