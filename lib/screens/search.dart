a6df630ee9dd00457184b44554b6214bfa8757d4
Novanaa <kadeknovacahayaputra@gmail.com>
2024-06-10 00:23:16 -0400

    Implemented search screen bar widget

--- lib/screens/search.dart
+++ lib/screens/search.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:musiku/sections/search/search_bar.dart';
 
 class SearchScreen extends StatelessWidget {
   const SearchScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text("search")));
+    return Scaffold(
+      body: ListView(
+        children: const [SearchScreenBar()],
+      ),
+    );
   }
 }
