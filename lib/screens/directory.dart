88e11fb1df2858cad6d76500f5b4e6860bafa827
Novanaa <kadeknovacahayaputra@gmail.com>
2024-06-10 11:21:12 -0400

    Added directory search screen widget

--- lib/screens/directory.dart
+++ lib/screens/directory.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:musiku/constants/color.dart';
 import 'package:musiku/sections/directory/header.dart';
 
@@ -11,6 +12,63 @@ class DirectoryScreen extends StatelessWidget {
       data: Theme.of(context).copyWith(splashColor: ColorConstants.splashColor),
       child: const Scaffold(
         appBar: DirectoryHeader(),
+      ),
+    );
+  }
+}
+
+class DirectorySearchScreen extends StatefulWidget {
+  const DirectorySearchScreen({super.key});
+
+  @override
+  State<DirectorySearchScreen> createState() => _DirectorySearchScreenState();
+}
+
+class _DirectorySearchScreenState extends State<DirectorySearchScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        foregroundColor: ColorConstants.iconColor,
+        title: directorySearchBar(),
+        titleSpacing: -0,
+      ),
+    );
+  }
+
+  // TODO: Implement directory search feature
+  void onTextfieldChanged(String text) {}
+
+  Container directorySearchBar() {
+    return Container(
+      padding: const EdgeInsets.only(right: 15),
+      child: TextField(
+        onChanged: onTextfieldChanged,
+        style: const TextStyle(
+            color: ColorConstants.textColor,
+            fontWeight: FontWeight.w500,
+            fontSize: 13.5),
+        cursorColor: ColorConstants.textColor,
+        decoration: InputDecoration(
+          filled: true,
+          fillColor: ColorConstants.inputColor,
+          prefixIcon: IconButton(
+            onPressed: () {},
+            icon: SvgPicture.asset("assets/icons/search.svg"),
+            color: ColorConstants.iconColor,
+          ),
+          hintText: "What are you looking for?",
+          hintStyle: const TextStyle(
+              color: ColorConstants.textColor,
+              fontWeight: FontWeight.w500,
+              fontSize: 13.5),
+          enabledBorder:
+              OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
+        ),
       ),
     );
   }
