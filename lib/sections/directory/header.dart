8f7104f8b2afc1392facdd2a2facf77190aa50d8
Novanaa <kadeknovacahayaputra@gmail.com>
2024-06-10 23:23:04 -0400

    Removed unused import module

--- lib/sections/directory/header.dart
+++ lib/sections/directory/header.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
-import 'package:get/get_core/src/get_main.dart';
 import 'package:get/route_manager.dart';
 import 'package:musiku/constants/color.dart';
 import 'package:musiku/screens/directory.dart';
