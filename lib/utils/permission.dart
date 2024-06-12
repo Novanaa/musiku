c044aeaa40a8ee86532ab58865fa2d237c40dd89
Novanaa <kadeknovacahayaputra@gmail.com>
2024-06-12 01:18:57 -0400

    Added get permission utils

--- lib/utils/permission.dart
+++ lib/utils/permission.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+import 'package:get/get.dart';
 import 'package:get/route_manager.dart';
+import 'package:musiku/screens/layout.dart';
 import 'package:musiku/screens/welcome.dart';
 import 'package:permission_handler/permission_handler.dart';
 
@@ -6,4 +9,20 @@ Future<void> checkPermission() async {
   bool isGranted = await Permission.storage.isGranted;
 
   if (!isGranted) Get.to(const WelcomeScreen());
+}
+
+Future<void> getPermission() async {
+  bool isGranted = await Permission.storage.isGranted;
+
+  if (!isGranted) {
+    PermissionStatus permissionStatus = await Permission.storage.request();
+
+    if (permissionStatus.isGranted) {
+      Get.offAll(const Layout(), transition: Transition.cupertino);
+    }
+
+    if (permissionStatus.isDenied) exit(0);
+
+    if (permissionStatus.isPermanentlyDenied) await openAppSettings();
+  }
 }
