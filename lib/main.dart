9c5c538ebf621904b39e0f956fdeabebda10cbc4
Novanaa <kadeknovacahayaputra@gmail.com>
2024-06-15 08:20:45 -0400

    Added flutter native splash screen when application data isn't loaded

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:musiku/constants/color.dart';
 import 'package:musiku/screens/layout.dart';
 import 'package:flutter/services.dart';
-import 'package:musiku/utils/permission.dart';
+import 'package:musiku/utils/app_init.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
       .then((value) => runApp(const MainApp()));
-  checkPermission();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  applicationInit().then((value) => FlutterNativeSplash.remove());
 }
 
 class MainApp extends StatelessWidget {
