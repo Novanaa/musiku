598a05c6819a2c899cae1dbbbf6de48e2e6b5d46
Novanaa <kadeknovacahayaputra@gmail.com>
2024-06-15 23:34:29 -0400

    Registered music and directory controller

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
-import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:get/get.dart';
 import 'package:musiku/constants/color.dart';
+import 'package:musiku/controller.dart';
 import 'package:musiku/screens/layout.dart';
 import 'package:flutter/services.dart';
 import 'package:musiku/utils/app_init.dart';
 
 void main() {
+  Get.put(MusicController());
+  Get.put(DirectoryController());
+
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
       .then((value) => runApp(const MainApp()));
