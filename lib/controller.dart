a774a27a21e0353d1124e447f3d2a21e20ace331
Novaa <130564362+Novanaa@users.noreply.github.com>
2024-06-15 23:36:27 -0400

    [MUS-32] Get User Music Folder/Directory (#46)
    * Added folder/diectory model
    
    * Added application directory controller
    
    * Registered music and directory controller
    
    * Excetuted get directory on application init
    
    * Removed unused import

--- lib/controller.dart
+++ lib/controller.dart
@@ -1,13 +1,38 @@
 import 'package:get/get.dart';
 import 'package:on_audio_query/on_audio_query.dart';
+import 'package:musiku/model.dart';
 
 class MusicController extends GetxController {
   final OnAudioQuery _audioQuery = OnAudioQuery();
   final RxList<SongModel> music = <SongModel>[].obs;
 
-  void getMusic() async {
+  Future<List<SongModel>> getMusic() async {
     List<SongModel> songs = await _audioQuery.querySongs();
     music.addAll(songs);
+    update();
+
+    return songs;
+  }
+}
+
+class DirectoryController extends GetxController {
+  final RxList<Directory> directory = <Directory>[].obs;
+  final MusicController _musicController = Get.find<MusicController>();
+
+  void getDirectory() async {
+    List<SongModel> music = await _musicController.getMusic();
+
+    List<Directory> mappedDirectories = music
+        .map((value) {
+          List<String> splitedPath = value.data.split("/");
+          splitedPath.removeLast();
+
+          return Directory(path: splitedPath.join("/"), name: splitedPath.last);
+        })
+        .toSet()
+        .toList();
+
+    directory.assignAll(mappedDirectories);
     update();
   }
 }
--- lib/utils/app_init.dart
+++ lib/utils/app_init.dart
@@ -4,9 +4,12 @@ import 'package:musiku/utils/permission.dart';
 
 Future<void> applicationInit() async {
   final MusicController musicController = Get.put(MusicController());
+  final DirectoryController directoryController =
+      Get.put(DirectoryController());
   bool isPermissionGranted = await checkPermission();
 
   if (isPermissionGranted) {
     musicController.getMusic();
+    directoryController.getDirectory();
   }
 }
