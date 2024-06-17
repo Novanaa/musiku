52526313962e3c6ecb43808ba268cb5510001192
Novanaa <kadeknovacahayaputra@gmail.com>
2024-06-17 09:57:45 -0400

    Added music player services

---
+++ lib/services/music_player.dart
@@ -0,0 +1,34 @@
+import 'package:just_audio/just_audio.dart';
+
+class MusicPlayer {
+  static final AudioPlayer _player = AudioPlayer();
+
+  static AudioPlayer getInstance() {
+    return _player;
+  }
+
+  static Future<void> load(String uri) async {
+    AudioSource audioSource = AudioSource.file(uri);
+    await _player.setAudioSource(audioSource);
+  }
+
+  static Future<void> play() async {
+    await _player.play();
+  }
+
+  static Future<void> pause() async {
+    await _player.pause();
+  }
+
+  static Future<void> seekTo(Duration position) async {
+    await _player.seek(position);
+  }
+
+  static Future<void> setRepeatMode(LoopMode mode) async {
+    await _player.setLoopMode(mode);
+  }
+
+  static bool isPlaying() {
+    return _player.playing;
+  }
+}
