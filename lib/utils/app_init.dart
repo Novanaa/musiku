724e3068a4d73b1515a024a62dd1d63b2fb094b6
Novanaa <kadeknovacahayaputra@gmail.com>
2024-06-18 04:06:32 -0400

    Removed load music on app init

--- lib/utils/app_init.dart
+++ lib/utils/app_init.dart
@@ -1,8 +1,6 @@
 import 'package:get/get.dart';
 import 'package:just_audio/just_audio.dart';
 import 'package:musiku/controller.dart';
-import 'package:musiku/model.dart';
-import 'package:musiku/repository/current_played.dart';
 import 'package:musiku/services/music_player.dart';
 import 'package:musiku/utils/permission.dart';
 
@@ -12,14 +10,8 @@ Future<void> applicationInit() async {
       Get.put(DirectoryController());
 
   bool isPermissionGranted = await checkPermission();
-  CurrentMusicPlayedModel? currentMusicPlayed =
-      await CurrentMusicPlayed.retrieve();
 
   await MusicPlayer.setRepeatMode(LoopMode.all);
-
-  if (currentMusicPlayed != null) {
-    MusicPlayer.load(currentMusicPlayed.music.data);
-  }
 
   if (isPermissionGranted) {
     musicController.getMusic();
