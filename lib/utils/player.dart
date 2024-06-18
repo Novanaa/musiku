ab874a950972e035f58d516b180eafcb075215a8
Novanaa <kadeknovacahayaputra@gmail.com>
2024-06-18 04:07:24 -0400

    Renamed current music played repository

--- lib/utils/player.dart
+++ lib/utils/player.dart
@@ -1,10 +1,15 @@
+import 'package:get/get.dart';
+import 'package:musiku/controller.dart';
 import 'package:musiku/model.dart';
-import 'package:musiku/repository/current_played.dart';
 import 'package:musiku/services/music_player.dart';
 import 'package:on_audio_query/on_audio_query.dart';
 
 Future<void> playMusic(SongModel music) async {
+  final CurrentMusicPlayedController currentMusicPLayedController =
+      Get.put(CurrentMusicPlayedController());
+
   await MusicPlayer.load(music.data);
   await MusicPlayer.play();
-  CurrentMusicPlayed.save(CurrentMusicPlayedModel(music: music, position: 0));
+  currentMusicPLayedController.setCurrentMusicPlayed(
+      CurrentMusicPlayedModel(music: music, position: 0));
 }
