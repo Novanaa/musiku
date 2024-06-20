4b0a4e44afe0182263adfbfca9c639bb92bf0242
Novanaa <kadeknovacahayaputra@gmail.com>
2024-06-20 02:44:38 -0400

    Removed default old loop mode

--- lib/utils/app_init.dart
+++ lib/utils/app_init.dart
@@ -1,7 +1,5 @@
 import 'package:get/get.dart';
-import 'package:just_audio/just_audio.dart';
 import 'package:musiku/controller.dart';
-import 'package:musiku/services/music_player.dart';
 import 'package:musiku/utils/permission.dart';
 
 Future<void> applicationInit() async {
@@ -10,8 +8,6 @@ Future<void> applicationInit() async {
       Get.put(DirectoryController());
 
   bool isPermissionGranted = await checkPermission();
-
-  await MusicPlayer.setRepeatMode(LoopMode.all);
 
   if (isPermissionGranted) {
     musicController.getMusic();
