9f0e5ed340c3f3f3d34e5b40bcca8fafba326d37
Novanaa <kadeknovacahayaputra@gmail.com>
2024-06-22 06:48:02 -0400

    Added repeat mode bottom sheet modal

--- lib/sections/home/bottom_sheet.dart
+++ lib/sections/home/bottom_sheet.dart
@@ -8,19 +8,14 @@ import 'package:musiku/model.dart';
 void openSortMusicBottomSheet(BuildContext context) {
   showModalBottomSheet(
       context: context,
-      builder: (context) => const SortMusicBottomSheet(),
+      builder: (context) => SortMusicBottomSheet(),
       isScrollControlled: true,
       backgroundColor: ColorConstants.modalBackgroundColor);
 }
 
-class SortMusicBottomSheet extends StatefulWidget {
-  const SortMusicBottomSheet({super.key});
+class SortMusicBottomSheet extends StatelessWidget {
+  SortMusicBottomSheet({super.key});
 
-  @override
-  State<SortMusicBottomSheet> createState() => _SortMusicBottomSheetState();
-}
-
-class _SortMusicBottomSheetState extends State<SortMusicBottomSheet> {
   final SortMusicController sortMusicController =
       Get.put(SortMusicController());
 
@@ -83,6 +78,75 @@ class _SortMusicBottomSheetState extends State<SortMusicBottomSheet> {
 
   void onChecked(SortMusicModel value) {
     sortMusicController.setSortMusicState(value.id);
+    Get.back();
+  }
+}
+
+void openRepeatModeBottomSheet(BuildContext context) {
+  showModalBottomSheet(
+      context: context,
+      builder: (context) => const RepeatModeDrawer(),
+      isScrollControlled: true,
+      backgroundColor: ColorConstants.modalBackgroundColor);
+}
+
+class RepeatModeDrawer extends StatelessWidget {
+  const RepeatModeDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 280,
+      padding: const EdgeInsets.only(top: 15, left: 10, right: 10),
+      width: MediaQuery.of(context).size.width,
+      child: Column(
+        children: [repeatModeDrawerHeader(), repeatModeDrawerOptionsList()],
+      ),
+    );
+  }
+
+  Container repeatModeDrawerHeader() {
+    return Container(
+      margin: const EdgeInsets.only(top: 10),
+      child: const Center(
+        child: Text("Music Repeat Mode",
+            textAlign: TextAlign.center,
+            style: TextStyle(fontWeight: FontWeight.w600, fontSize: 16)),
+      ),
+    );
+  }
+
+  Container repeatModeDrawerOptionsList() {
+    return Container(
+      margin: const EdgeInsets.only(top: 10),
+      child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: CommonConstants.repeatModeList
+              .map(
+                (value) => InkWell(
+                  onTap: () => onChecked(value),
+                  borderRadius: BorderRadius.circular(5),
+                  child: Row(
+                    children: [
+                      Radio(
+                        value: value.id,
+                        onChanged: (checkedId) {},
+                        groupValue: 1,
+                        activeColor: ColorConstants.textColor,
+                      ),
+                      Text(
+                        value.name,
+                        style: const TextStyle(fontWeight: FontWeight.w500),
+                      ),
+                    ],
+                  ),
+                ),
+              )
+              .toList()),
+    );
+  }
+
+  void onChecked(RepeatModeModel value) {
     Get.back();
   }
 }
