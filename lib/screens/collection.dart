47dfc911b627e513ece7fd0f7b05ae92bd9c58c0
Novanaa <kadeknovacahayaputra@gmail.com>
2024-06-25 06:17:42 -0400

    Added collection search screen

--- lib/screens/collection.dart
+++ lib/screens/collection.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:musiku/components/placeholder.dart';
 import 'package:musiku/constants/color.dart';
 import 'package:musiku/sections/collection/header.dart';
 
@@ -12,6 +14,72 @@ class CollectionScreen extends StatelessWidget {
       child: const Scaffold(
         body: Center(child: Text("collection")),
         appBar: CollectionHeader(),
+      ),
+    );
+  }
+}
+
+class CollectionSearchScreen extends StatefulWidget {
+  const CollectionSearchScreen({super.key});
+
+  @override
+  State<CollectionSearchScreen> createState() => _CollectionSearchScreenState();
+}
+
+class _CollectionSearchScreenState extends State<CollectionSearchScreen> {
+  String keyword = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: collectionSearchScreenAppBar(),
+      // TODO: Implement search user collection feature
+      body: const SearchWelcomeScreen(),
+    );
+  }
+
+  void onTextfieldChanged(String text) {
+    setState(() => keyword = text);
+  }
+
+  AppBar collectionSearchScreenAppBar() {
+    return AppBar(
+      backgroundColor: Colors.transparent,
+      foregroundColor: ColorConstants.iconColor,
+      title: collectionSearchBar(),
+      titleSpacing: -0,
+    );
+  }
+
+  Container collectionSearchBar() {
+    return Container(
+      padding: const EdgeInsets.only(right: 15),
+      child: TextField(
+        onChanged: onTextfieldChanged,
+        style: const TextStyle(
+            color: ColorConstants.textColor,
+            fontWeight: FontWeight.w500,
+            fontSize: 13.5),
+        cursorColor: ColorConstants.textColor,
+        decoration: InputDecoration(
+          filled: true,
+          fillColor: ColorConstants.inputColor,
+          prefixIcon: IconButton(
+            onPressed: () {},
+            icon: SvgPicture.asset("assets/icons/search.svg"),
+            color: ColorConstants.iconColor,
+          ),
+          hintText: "What are you looking for?",
+          hintStyle: const TextStyle(
+              color: ColorConstants.textColor,
+              fontWeight: FontWeight.w500,
+              fontSize: 13.5),
+          enabledBorder:
+              OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
+        ),
       ),
     );
   }
