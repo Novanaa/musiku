d2d947e4fb3c1a7c6e204877f686e75afdf6c26d
Novanaa <kadeknovacahayaputra@gmail.com>
2024-06-25 01:19:21 -0400

    Added on application terminated method

--- lib/utils/events.dart
+++ lib/utils/events.dart
@@ -1,7 +1,11 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:just_audio_background/just_audio_background.dart';
 import 'package:musiku/controller.dart';
+import 'package:musiku/model.dart';
+import 'package:musiku/services/music_player.dart';
 import 'package:musiku/utils/permission.dart';
+import 'package:on_audio_query/on_audio_query.dart';
 
 Future<void> onApplicationInit() async {
   final MusicController musicController = Get.put(MusicController());
@@ -19,5 +23,34 @@ Future<void> onApplicationInit() async {
       androidNotificationChannelName: 'Audio playback',
       androidNotificationOngoing: true,
     );
+  }
+}
+
+void onApplicationTerminated() {
+  final CurrentMusicPlayedController currentMusicPlayedController =
+      Get.put(CurrentMusicPlayedController());
+
+  WidgetsBinding.instance.addPostFrameCallback((_) {
+    WidgetsBinding.instance.addObserver(ApplicationEvents(onDetached: () {
+      if (currentMusicPlayedController.currentMusicPlayed.value != null) {
+        currentMusicPlayedController.setCurrentMusicPlayed(
+            CurrentMusicPlayedModel(
+                music: currentMusicPlayedController
+                    .currentMusicPlayed.value?.music as SongModel,
+                position: MusicPlayer.getInstance().position.inMilliseconds));
+      }
+    }));
+  });
+}
+
+class ApplicationEvents extends WidgetsBindingObserver {
+  VoidCallback? onDetached;
+  ApplicationEvents({this.onDetached});
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    super.didChangeAppLifecycleState(state);
+
+    if (state == AppLifecycleState.detached) onDetached!();
   }
 }
