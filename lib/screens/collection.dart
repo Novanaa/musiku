30165d411dfc1ec2432f215802ed9ee030b79e8c
Novaa <130564362+Novanaa@users.noreply.github.com>
2024-06-25 06:05:53 -0400

    [MUS-66] Create Collection Screen Header (#117)
    * Added plus svg icon
    
    * Implemented collection header component widget
    
    * Added collection header widget

--- lib/screens/collection.dart
+++ lib/screens/collection.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:musiku/constants/color.dart';
+import 'package:musiku/sections/collection/header.dart';
 
 class CollectionScreen extends StatelessWidget {
   const CollectionScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text("collection")));
+    return Theme(
+      data: Theme.of(context).copyWith(splashColor: ColorConstants.splashColor),
+      child: const Scaffold(
+        body: Center(child: Text("collection")),
+        appBar: CollectionHeader(),
+      ),
+    );
   }
 }
