eefab9d2aff6ad50905ec8c15acf5132583d0d88
Novanaa <kadeknovacahayaputra@gmail.com>
2024-06-25 22:26:16 -0400

    Added favorites music repository

---
+++ lib/repository/favorites_music.dart
@@ -0,0 +1,33 @@
+import 'dart:convert';
+
+import 'package:on_audio_query/on_audio_query.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class FavoritesMusicRepository {
+  static const String favoritesMusicKey = "favorites_music";
+
+  static Future<List<SongModel>> getFavoritesMusicState() async {
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    List<Map<String, dynamic>> jsonMap =
+        jsonDecode(preferences.getString(favoritesMusicKey) as String);
+
+    return List<SongModel>.from(
+        jsonMap.map((value) => SongModel(value)).toList());
+  }
+
+  static void addFavoritesMusic(SongModel song) async {
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    List<SongModel> favoritesMusicState = await getFavoritesMusicState();
+
+    favoritesMusicState.add(song);
+    preferences.setString(favoritesMusicKey,
+        jsonEncode(favoritesMusicState.map((value) => value.getMap).toList()));
+  }
+
+  static void init() async {
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    if (preferences.getString(favoritesMusicKey) == null) {
+      preferences.setString(favoritesMusicKey, jsonEncode(<SongModel>[]));
+    }
+  }
+}
