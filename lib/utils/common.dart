a19f01a90399bd728e40e80d48d9b52b26e57bd7
Novanaa <kadeknovacahayaputra@gmail.com>
2024-06-26 01:07:50 -0400

    Realocated parse duration utils method

--- lib/utils/common.dart
+++ lib/utils/common.dart
@@ -43,3 +43,13 @@ String getDate(int unixEpoch) {
 String parseFileSize(int bytes) {
   return "${(bytes / (1024 * 1024)).round()} MB";
 }
+
+String parseDuration(int duration) {
+  Duration convertedDuration = Duration(milliseconds: duration);
+  String durationInMinutes = convertedDuration.inMinutes < 10
+      ? "0${convertedDuration.inMinutes}"
+      : convertedDuration.inMinutes.toString();
+  int durationInSeconds = duration % 60.ceil();
+
+  return "$durationInMinutes:${durationInSeconds < 10 ? "0$durationInSeconds" : durationInSeconds}";
+}
--- lib/utils/parse_duration.dart
+++
@@ -1,9 +0,0 @@
-String parseDuration(int duration) {
-  Duration convertedDuration = Duration(milliseconds: duration);
-  String durationInMinutes = convertedDuration.inMinutes < 10
-      ? "0${convertedDuration.inMinutes}"
-      : convertedDuration.inMinutes.toString();
-  int durationInSeconds = duration % 60.ceil();
-
-  return "$durationInMinutes:${durationInSeconds < 10 ? "0$durationInSeconds" : durationInSeconds}";
-}
