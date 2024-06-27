b26525a711906522ad6ae3c64de4c906acd44529
Novanaa <kadeknovacahayaputra@gmail.com>
2024-06-27 03:47:08 -0400

    Refactored some line of code

--- lib/components/collection_item.dart
+++ lib/components/collection_item.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:musiku/constants/color.dart';
+import 'package:musiku/model.dart' as model;
+import 'package:on_audio_query/on_audio_query.dart';
 
 class CollectionItem extends StatelessWidget {
   final VoidCallback onTap;
@@ -71,6 +73,29 @@ class CollectionItem extends StatelessWidget {
           ),
         )
       ],
+    );
+  }
+}
+
+class CollectionItemList {
+  static CollectionItem artist(ArtistModel artist) {
+    return CollectionItem(
+      title: artist.artist == "<unknown>" ? "Unkown artist" : artist.artist,
+      borderRadius: 1000,
+      description: "Artist",
+      // TODO: Implement view single artist feature
+      onTap: () {},
+      iconPath: "assets/icons/artist.svg",
+    );
+  }
+
+  static CollectionItem playlist(model.PlaylistModel playlist) {
+    return CollectionItem(
+      title: playlist.title,
+      description: "Playlist",
+      // TODO: Implement view single playlist feature
+      onTap: () {},
+      iconPath: "assets/icons/playlist.svg",
     );
   }
 }
