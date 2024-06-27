b52b226b5ffd72fc384c2e76194ee8eff8fb1d2a
Novanaa <kadeknovacahayaputra@gmail.com>
2024-06-26 22:02:05 -0400

    Added favorites music list component

--- lib/screens/favorites.dart
+++ lib/screens/favorites.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:musiku/components/music.dart';
+import 'package:musiku/components/placeholder.dart';
 import 'package:musiku/constants/color.dart';
+import 'package:musiku/controller.dart';
 
 class FavoritesMusicScreen extends StatelessWidget {
   const FavoritesMusicScreen({super.key});
@@ -8,6 +12,7 @@ class FavoritesMusicScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: favoritesMusicScreenAppBar(),
+      body: favoritesMusicList(),
     );
   }
 
@@ -22,6 +27,24 @@ class FavoritesMusicScreen extends StatelessWidget {
           fontSize: 17),
       backgroundColor: ColorConstants.backgroundColor,
       foregroundColor: ColorConstants.textColor,
+    );
+  }
+
+  Widget favoritesMusicList() {
+    final FavoritesMusicController favoritesMusicController =
+        Get.put(FavoritesMusicController());
+
+    if (favoritesMusicController.favoritesMusic.isEmpty) {
+      return const EmptyMusicPlaceholder();
+    }
+
+    return Obx(
+      () => ListView.builder(
+        padding: const EdgeInsets.only(bottom: 10),
+        itemBuilder: (BuildContext context, int index) =>
+            Music(song: favoritesMusicController.favoritesMusic[index]),
+        itemCount: favoritesMusicController.favoritesMusic.length,
+      ),
     );
   }
 }
