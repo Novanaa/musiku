d0bad027a842678f0c9f8ffe04215cfe0de396a5
Novanaa <kadeknovacahayaputra@gmail.com>
2024-06-30 00:15:05 -0400

    Added add playlist actions utils

--- lib/utils/actions.dart
+++ lib/utils/actions.dart
@@ -2,8 +2,10 @@ import 'dart:io';
 
 import 'package:get/get.dart';
 import 'package:musiku/controller.dart';
+import 'package:musiku/screens/collection.dart';
 import 'package:musiku/utils/common.dart';
 import 'package:on_audio_query/on_audio_query.dart';
+import 'package:musiku/model.dart' as model;
 
 void deleteMusic(String path) {
   try {
@@ -25,4 +27,22 @@ void unfavoriteMusic(SongModel song) {
   final FavoritesMusicController favoritesMusicController =
       Get.put(FavoritesMusicController());
   favoritesMusicController.unfavoriteMusic(song);
+}
+
+void addPlaylist(String playlistTitle) {
+  final PlaylistController playlistController = Get.put(PlaylistController());
+
+  if (playlistTitle.isEmpty || playlistTitle.trim().isEmpty) {
+    Get.back();
+    showToast("Playlist title should be filled.");
+    return;
+  }
+
+  Get.to(AddPlaylistSongsScreen(), transition: Transition.cupertino);
+  showToast("Sucessfully added playlist.");
+  playlistController.setPlaylistState(model.PlaylistModel(
+      id: generatePlaylistId(),
+      title: playlistTitle,
+      totalSongs: 0,
+      songs: []));
 }
