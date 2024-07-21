ae7f78c2ad0eee7dd75b8cabc9011dcc97e58e70
Novanaa <kadeknovacahayaputra@gmail.com>
2024-07-21 11:06:09 +0700

    Added custom icon size for custom icon and custom icon button

--- lib/components/icon.dart
+++ lib/components/icon.dart
@@ -5,13 +5,21 @@ class CustomIconButton extends StatelessWidget {
   final IconData data;
   final VoidCallback onPressed;
   final Color? color;
+  final double? size;
   const CustomIconButton(
-      {super.key, required this.data, required this.onPressed, this.color});
+      {super.key,
+      required this.data,
+      required this.onPressed,
+      this.color,
+      this.size});
 
   @override
   Widget build(BuildContext context) {
     return IconButton(
-      icon: Icon(data),
+      icon: Icon(
+        data,
+        size: size ?? 25,
+      ),
       onPressed: onPressed,
       color: color ?? ColorConstants.iconColor,
     );
@@ -21,13 +29,20 @@ class CustomIconButton extends StatelessWidget {
 class CustomIcon extends StatelessWidget {
   final IconData data;
   final Color? color;
-  const CustomIcon({super.key, required this.data, this.color});
+  final double? size;
+  const CustomIcon({
+    super.key,
+    required this.data,
+    this.color,
+    this.size,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Icon(
       data,
       color: color ?? ColorConstants.iconColor,
+      size: size ?? 25,
     );
   }
 }
